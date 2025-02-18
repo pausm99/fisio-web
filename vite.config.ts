d34c84a944f8b0d35b56b5db90db0c878b15a2d1
pausm99 <pausm99@gmail.com>
2025-02-18 19:24:07 +0100

    fix: vercel dploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: '/',
+  plugins: [vue()],
+  base: "/",
+  build: {
+    outDir: "dist", // La carpeta de salida (Vercel usa "dist" por defecto)
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
